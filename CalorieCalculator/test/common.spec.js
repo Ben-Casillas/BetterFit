0c527b77b71aaa5f0bb46371a77fd628d0aae093
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2024-12-31 21:00:50 -0800

    building out the testing, need to break away for secrets management

--- CalorieCalculator/test/common.spec.js
+++ CalorieCalculator/test/common.spec.js
@@ -1,54 +1,28 @@
-const supertest = require('supertest');
+const req = require('supertest')(require('../app.js'));
 const common = require('../api/routes/commons.js');
-// const request = supertest(common);
-const app = require('supertest')(require('../app.js'))
+const should = require("should");
 
-
-
-const sum = require("./sum.js");
-// var assert = require('assert');
-var chai = require("chai");
-var expect = chai.expect;
-//THIS IS MOCHA DING DO
-
-describe('Test', () => { //test suite, title of test suite, ( collection fo realted test cases)
+describe("Common Test Suite", () => { //testset
     
-    it("Test the sum method", () => {
-        expect(sum(1, 2)).to.be.equal(3);
+    it("Testing area is fully functional SIR", async () => { //test case
+        const response = await req.get('/common/healthCheck');
+        response.status.should.equal(200);
+        response.body.should.have.property("Im still standing", "better than I ever did"); //what did it respond with
     });
-    // describe() //individual test cases
-});
 
-describe("Common Test Suite", () => {
-    
-    it("Responds to the API successfully", async () => {
-        // const response = await request.post('/common/query').send();
-        // expect(response.status).to.be.equal(200)
-        return app.get('/common/healthCheck').expect(200);
+    it("Common endpoint with invalid query fails", async () => {
+        const response = await req.get('/common/query/VeryCoolApplication');
+        response.status.should.equal(500);
+        response.body.should.have.property("error", "Internal Server Error");
     });
+
+    it("Common endpoint with valid query returns correct information", async () => {
+        const response = await req.get('/common/query/grapes');
+        response.status.should.equal(500);
+        var test = response.body
+
+    // response.body.should.have.property("error", "Internal Server Error")
+    // const test = response.body;
+    });
+
 });
-
-
-
-
-
-
-
-
-//-----------
-
-// describe("POST", "/specific/:amount", () =>{
-//     describe("When a valid amount is provided", ()=>{
-//         it("should respond with status 200 and return JSON data", async ()=>{
-//         const requestBody = {
-//             "query":"cheeseburger"
-//         };
-//         const amount = 2;
-//         const response = await request.post(`/specific/${amount}`).send(requestBody);
-//         expect(response.status).toBe(200);
-//         expect(response.type).toMatch(/json/);
-//         });
-//     });
-// });
-
-// Test("Specific - When a valid amount is provided")
